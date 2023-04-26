db1fac01c23ef9a475c167ae3ab8446a11a1866a
Arindam Karmakar <arindamkarmakar.ak47.digital@gmail.com>
2023-04-27 04:31:53 +0530

    modified upload_sketch.sh

--- upload_sketch.sh
+++ upload_sketch.sh
@@ -1,5 +1,9 @@
 cd src/arduino
 
 arduino-cli compile -b arduino:avr:uno driver_module
+echo "Sketch compiled successfully !!!"
+
+echo "Uploading sketch ..."
 
 arduino-cli upload -b arduino:avr:uno -p /dev/ttyACM0 driver_module
+echo "Sketch uploaded to Arduino UNO successfully !!!"
