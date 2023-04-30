56c49c1c6b4ac2f128d25f3b6ad8d243d23c1098
Arindam Karmakar <arindamkarmakar.ak47.digital@gmail.com>
2023-04-30 14:09:52 +0530

    added pause event in rPi car_control

--- src/rPi/car_control.py
+++ src/rPi/car_control.py
@@ -1,4 +1,6 @@
 import socket
+import time
+from threading import Thread, Event
 
 import sys
 from path import Path
@@ -8,8 +10,15 @@ sys.path.append(coreDir.abspath())
 
 from core.utils import *
 
+pause_event = Event()
+
 class CarControl:
     def __init__(self) -> None:
+        pause_event.set()
+
+        pause_thread = Thread(target=self.control_pause_event)
+        pause_thread.start()
+
         try:
             self.ser = find_arduino(serial_number=arduino_serial_number)
         except IOError as e:
@@ -18,6 +27,22 @@ class CarControl:
 
         self.ser.flush()
 
+    def control_pause_event(self):
+        while True:
+            is_set = pause_event.is_set()
+            print("pause state ===>>> ", is_set)
+
+            time.sleep(0.7)
+
+            if(is_set):
+                pause_event.clear()
+            else:
+                pause_event.set()
+
+    def stop_car(self):
+        self.ser.write(b'0')
+
+
     def process_control_input(self) -> None:
         control_socket = socket.socket()
         control_socket.connect(control_data_stream_address)
@@ -25,8 +50,11 @@ class CarControl:
         try:
             while True:
                 data = control_socket.recv(1024)
-                print("Control ===>>> ", data)
 
+                if(pause_event.is_set()):
+                    self.stop_car()
+                    continue 
+                
                 if (data == b'Left'):
                     self.ser.write(b'4')
                 elif (data == b'Right'):
@@ -34,7 +62,7 @@ class CarControl:
                 elif (data == b'Forward'):
                     self.ser.write(b'1')
                 else:
-                    self.ser.write(b'0')
+                    self.stop_car()
         finally:
             print('Exit')
             control_socket.close()
